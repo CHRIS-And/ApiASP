516bf8a9aebbe4e6ac1826ae9c37d0a54244f981
christopherAA1 <calcantar2@ucol.mx>
2025-01-07 11:01:57 -0600

    rebase hecho

---
+++ Controllers/PrincipalController.cs
@@ -0,0 +1,297 @@
+﻿using ApiAsp.Entities;
+using ApiAsp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Http;
+
+namespace ApiAsp.Controllers
+{
+    [RoutePrefix("api/principal")]
+    public class PrincipalController : ApiController
+    {
+        [HttpPost]
+        [Route("demo")]
+        public IHttpActionResult demo([FromBody] datos datos)
+        {
+            int IdJson = datos.IdJson.ToString() == "" ? 0 : datos.IdJson; 
+            string DatJson = datos.DatJson.ToString() == "" ? "0" : datos.DatJson.ToString();
+
+            dynamic Response = null;
+            dynamic Retorno = null;
+            try
+            {
+                Retorno = Principal.Registrar(IdJson, DatJson);
+
+                Response = new
+                {
+                    Estado = true,
+                    Mensaje = Retorno,
+                };
+
+                return Ok(Response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error general");
+            }
+
+        }
+
+        [HttpPost]
+        [Route("demo2")]
+        public IHttpActionResult demo2([FromBody] datos datos)
+        {
+            int IdJson = datos.IdJson.ToString() == "" ? 0 : datos.IdJson;
+            string DatJson = datos.DatJson.ToString() == "" ? "0" : datos.DatJson.ToString();
+
+            dynamic Response = null;
+            dynamic Retorno = null;
+            try
+            {
+                Retorno = Principal.Registrar2(IdJson, DatJson);
+
+                Response = new
+                {
+                    Estado = true,
+                    Mensaje = Retorno,
+                };
+
+                return Ok(Response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error general");
+            }
+
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public IHttpActionResult create([FromBody] personaDatos datos)
+        {
+            if (datos.Nombre != null && datos.ApellidoP != null && datos.Telefono.ToString().Length == 10 )
+            {
+                string[] DatJson = new string[4];
+                string Nombre = datos.Nombre.ToString() == "" ? "0" : datos.Nombre.ToString();
+                string ApellidoP = datos.ApellidoP.ToString() == "" ? "0" : datos.ApellidoP.ToString();
+                string ApellidoM = string.IsNullOrEmpty(datos.ApellidoM?.ToString())  ? " " : datos.ApellidoM.ToString();
+                string Direccion = string.IsNullOrEmpty(datos.Direccion?.ToString()) ? " " : datos.Direccion.ToString();
+                long Telefono = datos.Telefono.ToString() == "" ? 0 : datos.Telefono;
+
+                DatJson[0] = Nombre;
+                DatJson[1] = ApellidoP;
+                DatJson[2] = ApellidoM;
+                DatJson[3] = Direccion;
+
+                dynamic Response = null;
+                dynamic Retorno = null;
+                try
+                {
+                    Retorno = Principal.Registrar3(Telefono, DatJson);
+
+                    Response = new
+                    {
+                        Estado = true,
+                        Mensaje = Retorno,
+                    };
+
+                    return Ok(Response);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Error general");
+                }
+            }
+            else
+                return BadRequest("Error");
+        }
+
+        [HttpPost]
+        [Route("update")]
+        public IHttpActionResult update([FromBody] personaDatos datos)
+        {
+            if (datos.Nombre != null && datos.ApellidoP != null && datos.Telefono.ToString().Length == 10)
+            {
+                string[] DatJson = new string[4];
+                long[] NumJson = new long[2];
+                int Id = datos.Id.ToString() == "" ? 0 : datos.Id;
+                string Nombre = datos.Nombre.ToString() == "" ? "0" : datos.Nombre.ToString();
+                string ApellidoP = datos.ApellidoP.ToString() == "" ? "0" : datos.ApellidoP.ToString();
+                string ApellidoM = datos.ApellidoM.ToString() == "" ? "0" : datos.ApellidoM.ToString();
+                string Direccion = datos.Direccion.ToString() == "" ? "0" : datos.Direccion.ToString();
+                long Telefono = datos.Telefono.ToString() == "" ? 0 : datos.Telefono;
+
+                NumJson[0] = Id;
+                NumJson[1] = Telefono;
+
+                DatJson[0] = Nombre;
+                DatJson[1] = ApellidoP;
+                DatJson[2] = ApellidoM;
+                DatJson[3] = Direccion;
+
+                dynamic Response = null;
+                dynamic Retorno = null;
+                try
+                {
+                    Retorno = Principal.Registrar4(NumJson, DatJson);
+
+                    Response = new
+                    {
+                        Estado = true,
+                        Mensaje = Retorno,
+                    };
+
+                    return Ok(Response);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Error general");
+                }
+
+            } else
+               return BadRequest("Error");
+        }
+
+        [HttpPost]
+        [Route("deactivate")]
+        public IHttpActionResult deactivate([FromBody] personaDatos datos)
+        {
+            int Id = datos.Id.ToString() == "" ? 0 : datos.Id;
+
+            dynamic Response = null;
+            dynamic Retorno = null;
+
+            try
+            {
+                Retorno = Principal.Registrar5(Id);
+
+                Response = new
+                {
+                    Estado = true,
+                    Mensaje = Retorno,
+                };
+
+                return Ok(Retorno);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error general");
+            }
+
+        }
+
+        [HttpPost]
+        [Route("table")]
+        public IHttpActionResult table([FromBody] datos datos)
+        {
+            int Tabla = datos.Tabla.ToString() == "" ? 0 : datos.Tabla;
+
+            dynamic Response = null;
+            dynamic Retorno = null;
+            try
+            {
+                Retorno = Principal.verTabla(Tabla);
+
+                Response = new
+                {
+                    Estado = true,
+                    Mensaje = Retorno,
+                };
+
+                return Ok(Response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error general");
+            }
+
+        }
+
+        [HttpPost]
+        [Route("search")]
+        public IHttpActionResult search([FromBody] datos datos)
+        {
+            string Busqueda = datos.Busqueda.ToString() == "" ? "0" : datos.Busqueda.ToString();
+
+            dynamic Response = null;
+            dynamic Retorno = null;
+            try
+            {
+                Retorno = Principal.verResultado(Busqueda);
+
+                Response = new
+                {
+                    Estado = true,
+                    Mensaje = Retorno,
+                };
+
+                return Ok(Response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error general");
+            }
+
+        }
+
+        [HttpPost]
+        [Route("seleccionar")]
+        public IHttpActionResult seleccionar([FromBody] personaDatos datos)
+        {
+            int Id = datos.Id.ToString() == "" ? 0 : datos.Id;
+
+            dynamic Response = null;
+            dynamic Retorno = null;
+
+            try
+            {
+                Retorno = Principal.SeleccionarPersona(Id);
+
+                Response = new
+                {
+                    Estado = true,
+                    Mensaje = Retorno,
+                };
+
+                return Ok(Retorno);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error general");
+            }
+        }
+
+        [HttpPost]
+        [Route("persona")]
+        public IHttpActionResult persona([FromBody] personaDatos datos)
+        {
+            int Estatus = datos.Estatus.ToString() == "" ? 0 : datos.Estatus;
+
+            dynamic Response = null;
+            dynamic Retorno = null;
+            try
+            {
+                Retorno = Principal.verPersona(Estatus);
+
+                Response = new
+                {
+                    Estado = true,
+                    Mensaje = Retorno,
+                };
+
+                return Ok(Response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error general");
+            }
+
+        }
+
+    }
+}
---
+++ Models/Principal.cs
@@ -0,0 +1,397 @@
+﻿using ApiAsp.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ApiAsp.Models
+{
+    public class Principal
+    {
+        public int Numero { get; set; }
+        public string Mensaje { get; set; }
+
+        static public DataTable Registrar(int Numero, string Mensaje)
+        {
+            BaseDatos _BaseDatos = new BaseDatos();
+            DataTable _DataTable = new DataTable();
+
+            string SentenciaSQL = "EXEC ChrisSaludo @Numero, @Mensaje";
+            List<SqlParameter> Parametros = new List<SqlParameter>
+            {
+                new SqlParameter("@Numero", SqlDbType.Int) { Value = Numero },
+                new SqlParameter("@Mensaje", SqlDbType.VarChar) { Value = Mensaje }
+            };
+
+            _DataTable = _BaseDatos.ObtenerDataTable(SentenciaSQL, Parametros);
+
+            if (_DataTable != null && _DataTable.Rows.Count > 0)
+            {
+                return _DataTable;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        static public respuestaRequest Registrar2(int Numero, string Mensaje)
+        {
+            respuestaRequest rRequest = new respuestaRequest();
+            SqlClass miSqlClass = new SqlClass();
+            miSqlClass.conectar();
+
+            string JSONString = string.Empty;
+            DataTable dtInfo = new DataTable();
+
+
+            //pasamos los parametros para mandarlos al sp
+            List<parametro> parametros = new List<parametro>
+            {
+                new parametro() {nombre = "Numero", valor = Numero.ToString()},
+                new parametro() {nombre = "Mensaje", valor =  Mensaje},
+
+            };
+
+            //parametro de salida del sp
+            List<parametro> parametrosOutput = new List<parametro>
+            {
+                new parametro() {nombre = "Err", tipoValor = parametro.tipoValorSalida.int_},
+                new parametro() {nombre = "ErrDescripcion", tipoValor = parametro.tipoValorSalida.string_}
+            };
+
+
+            miSqlClass.funcStoreProcedureConsult("ChrisSaludo2", ref dtInfo, parametros, parametrosOutput);
+
+            if (parametrosOutput[0].valor == "1")
+            {
+                rRequest.Err = parametrosOutput[0].valor;
+                rRequest.ErrDescripcion = parametrosOutput[1].valor;
+            }
+
+
+            if (dtInfo.Rows.Count > 0)
+            {
+                JSONString = JsonConvert.SerializeObject(dtInfo);
+                rRequest.json = JSONString;
+            }
+
+            return rRequest;
+        }
+
+        static public respuestaRequest Registrar3(long Numero, string[] Datos)
+        {
+            respuestaRequest rRequest = new respuestaRequest();
+            SqlClass miSqlClass = new SqlClass();
+            miSqlClass.conectar();
+
+            string JSONString = string.Empty;
+            DataTable dtInfo = new DataTable();
+
+
+            //pasamos los parametros para mandarlos al sp
+            List<parametro> parametros = new List<parametro>
+            {
+                new parametro() {nombre = "Nombre",   valor =  Datos[0]},
+                new parametro() {nombre = "ApellidoP", valor =  Datos[1]},
+                new parametro() {nombre = "ApellidoM", valor =  Datos[2]},
+                new parametro() {nombre = "Direccion", valor =  Datos[3]},
+                new parametro() {nombre = "Telefono", valor = Numero.ToString()}
+
+            };
+
+            //parametro de salida del sp
+            List<parametro> parametrosOutput = new List<parametro>
+            {
+                new parametro() {nombre = "Err", tipoValor = parametro.tipoValorSalida.int_},
+                new parametro() {nombre = "ErrDescripcion", tipoValor = parametro.tipoValorSalida.string_}
+            };
+
+
+            miSqlClass.funcStoreProcedureConsult("Chris_Crear", ref dtInfo, parametros, parametrosOutput);
+
+            if (parametrosOutput[0].valor == "1")
+            {
+                rRequest.Err = parametrosOutput[0].valor;
+                rRequest.ErrDescripcion = parametrosOutput[1].valor;
+            }
+
+            if (parametrosOutput[0].valor == "3")
+            {
+                rRequest.Err = parametrosOutput[0].valor;
+                rRequest.ErrDescripcion = parametrosOutput[1].valor;
+            }
+
+            if (dtInfo.Rows.Count > 0)
+            {
+                // Regresar Lista
+                JSONString = JsonConvert.SerializeObject(dtInfo);
+                rRequest.json = JSONString;
+            }
+
+            return rRequest;
+        }
+
+        static public respuestaRequest Registrar4(long[] Numero, string[] Datos)
+        {
+            respuestaRequest rRequest = new respuestaRequest();
+            SqlClass miSqlClass = new SqlClass();
+            miSqlClass.conectar();
+
+            string JSONString = string.Empty;
+            DataTable dtInfo = new DataTable();
+
+
+            //pasamos los parametros para mandarlos al sp
+            List<parametro> parametros = new List<parametro>
+            {
+                new parametro() {nombre = "Id",        valor = Numero[0].ToString() },
+                new parametro() {nombre = "Nombre",   valor =  Datos[0]},
+                new parametro() {nombre = "ApellidoP", valor =  Datos[1]},
+                new parametro() {nombre = "ApellidoM", valor =  Datos[2]},
+                new parametro() {nombre = "Direccion", valor =  Datos[3]},
+                new parametro() {nombre = "Telefono",  valor = Numero[1].ToString()}
+
+            };
+
+            //parametro de salida del sp
+            List<parametro> parametrosOutput = new List<parametro>
+            {
+                new parametro() {nombre = "Err", tipoValor = parametro.tipoValorSalida.int_},
+                new parametro() {nombre = "ErrDescripcion", tipoValor = parametro.tipoValorSalida.string_}
+            };
+
+
+            miSqlClass.funcStoreProcedureConsult("Chris_Update", ref dtInfo, parametros, parametrosOutput);
+
+            if (parametrosOutput[0].valor == "1")
+            {
+                rRequest.Err = parametrosOutput[0].valor;
+                rRequest.ErrDescripcion = parametrosOutput[1].valor;
+            }
+
+            if (parametrosOutput[0].valor == "3")
+            {
+                rRequest.Err = parametrosOutput[0].valor;
+                rRequest.ErrDescripcion = parametrosOutput[1].valor;
+            }
+
+            if (dtInfo.Rows.Count > 0)
+            {
+                // Regresar Lista
+                JSONString = JsonConvert.SerializeObject(dtInfo);
+                rRequest.json = JSONString;
+            }
+
+            return rRequest;
+        }
+
+        static public respuestaRequest Registrar5(int Numero)
+        {
+            respuestaRequest rRequest = new respuestaRequest();
+            SqlClass miSqlClass = new SqlClass();
+            miSqlClass.conectar();
+
+            string JSONString = string.Empty;
+            DataTable dtInfo = new DataTable();
+
+
+            //pasamos los parametros para mandarlos al sp
+            List<parametro> parametros = new List<parametro>
+            {
+                new parametro() {nombre = "Id", valor = Numero.ToString()},
+            };
+
+            //parametro de salida del sp
+            List<parametro> parametrosOutput = new List<parametro>
+            {
+                new parametro() {nombre = "Err", tipoValor = parametro.tipoValorSalida.int_},
+                new parametro() {nombre = "ErrDescripcion", tipoValor = parametro.tipoValorSalida.string_}
+            };
+
+
+            miSqlClass.funcStoreProcedureConsult("Chris_Deactivate", ref dtInfo, parametros, parametrosOutput);
+
+            if (parametrosOutput[0].valor == "1")
+            {
+                rRequest.Err = parametrosOutput[0].valor;
+                rRequest.ErrDescripcion = parametrosOutput[1].valor;
+            }
+
+            if (dtInfo.Rows.Count > 0)
+            {
+                JSONString = JsonConvert.SerializeObject(dtInfo);
+                rRequest.json = JSONString;
+            }
+
+            return rRequest;
+        }
+
+        static public respuestaRequest verTabla(int Numero)
+        {
+            respuestaRequest rRequest = new respuestaRequest();
+            SqlClass miSqlClass = new SqlClass();
+            miSqlClass.conectar();
+
+            string JSONString = string.Empty;
+            DataTable dtInfo = new DataTable();
+
+
+            //pasamos los parametros para mandarlos al sp
+            List<parametro> parametros = new List<parametro>
+            {
+                new parametro() {nombre = "Filtro", valor = Numero.ToString()},
+            };
+
+            //parametro de salida del sp
+            List<parametro> parametrosOutput = new List<parametro>
+            {
+                new parametro() {nombre = "Err", tipoValor = parametro.tipoValorSalida.int_},
+                new parametro() {nombre = "ErrDescripcion", tipoValor = parametro.tipoValorSalida.string_}
+            };
+
+
+            miSqlClass.funcStoreProcedureConsult("Chris_Tabla", ref dtInfo, parametros, parametrosOutput);
+
+            if (parametrosOutput[0].valor == "1")
+            {
+                rRequest.Err = parametrosOutput[0].valor;
+                rRequest.ErrDescripcion = parametrosOutput[1].valor;
+            }
+
+            if (dtInfo.Rows.Count > 0)
+            {
+                JSONString = JsonConvert.SerializeObject(dtInfo);
+                rRequest.json = JSONString;
+            }
+
+            return rRequest;
+        }
+
+        static public respuestaRequest verResultado(string Busqueda)
+        {
+            respuestaRequest rRequest = new respuestaRequest();
+            SqlClass miSqlClass = new SqlClass();
+            miSqlClass.conectar();
+
+            string JSONString = string.Empty;
+            DataTable dtInfo = new DataTable();
+
+
+            //pasamos los parametros para mandarlos al sp
+            List<parametro> parametros = new List<parametro>
+            {
+                new parametro() {nombre = "Busqueda", valor = Busqueda},
+            };
+
+            //parametro de salida del sp
+            List<parametro> parametrosOutput = new List<parametro>
+            {
+                new parametro() {nombre = "Err", tipoValor = parametro.tipoValorSalida.int_},
+                new parametro() {nombre = "ErrDescripcion", tipoValor = parametro.tipoValorSalida.string_}
+            };
+
+
+            miSqlClass.funcStoreProcedureConsult("Chris_Search", ref dtInfo, parametros, parametrosOutput);
+
+            if (parametrosOutput[0].valor == "1")
+            {
+                rRequest.Err = parametrosOutput[0].valor;
+                rRequest.ErrDescripcion = parametrosOutput[1].valor;
+            }
+
+            if (dtInfo.Rows.Count > 0)
+            {
+                JSONString = JsonConvert.SerializeObject(dtInfo);
+                rRequest.json = JSONString;
+            }
+
+            return rRequest;
+        }
+
+        static public respuestaRequest verPersona(int Estatus)
+        {
+            respuestaRequest rRequest = new respuestaRequest();
+            SqlClass miSqlClass = new SqlClass();
+            miSqlClass.conectar();
+
+            string JSONString = string.Empty;
+            DataTable dtInfo = new DataTable();
+
+
+            //pasamos los parametros para mandarlos al sp
+            List<parametro> parametros = new List<parametro>
+            {
+                new parametro() {nombre = "estatus", valor = Estatus.ToString()},
+            };
+
+            //parametro de salida del sp
+            List<parametro> parametrosOutput = new List<parametro>
+            {
+                new parametro() {nombre = "Err", tipoValor = parametro.tipoValorSalida.int_},
+                new parametro() {nombre = "ErrDescripcion", tipoValor = parametro.tipoValorSalida.string_}
+            };
+
+
+            miSqlClass.funcStoreProcedureConsult("Ver_Persona", ref dtInfo, parametros, parametrosOutput);
+
+            if (parametrosOutput[0].valor == "1")
+            {
+                rRequest.Err = parametrosOutput[0].valor;
+                rRequest.ErrDescripcion = parametrosOutput[1].valor;
+            }
+
+            if (dtInfo.Rows.Count > 0)
+            {
+                JSONString = JsonConvert.SerializeObject(dtInfo);
+                rRequest.json = JSONString;
+            }
+
+            return rRequest;
+        }
+
+        static public respuestaRequest SeleccionarPersona(int Id)
+        {
+
+            respuestaRequest rRequest = new respuestaRequest();
+            SqlClass miSqlClass = new SqlClass();
+            miSqlClass.conectar();
+
+            string JSONString = string.Empty;
+            DataTable dtInfo = new DataTable();
+
+
+            //pasamos los parametros para mandarlos al sp
+            List<parametro> parametros = new List<parametro>
+            {
+                new parametro() {nombre = "Id", valor = Id.ToString()},
+            };
+
+            //parametro de salida del sp
+            List<parametro> parametrosOutput = new List<parametro>
+            {
+                new parametro() {nombre = "Err", tipoValor = parametro.tipoValorSalida.int_},
+                new parametro() {nombre = "ErrDescripcion", tipoValor = parametro.tipoValorSalida.string_}
+            };
+
+
+            miSqlClass.funcStoreProcedureConsult("Seleccionar_Persona", ref dtInfo, parametros, parametrosOutput);
+
+            if (parametrosOutput[0].valor == "1")
+            {
+                rRequest.Err = parametrosOutput[0].valor;
+                rRequest.ErrDescripcion = parametrosOutput[1].valor;
+            }
+
+            if (dtInfo.Rows.Count > 0)
+            {
+                JSONString = JsonConvert.SerializeObject(dtInfo);
+                rRequest.json = JSONString;
+            }
+
+            return rRequest;
+
+        }
+    }
+}
